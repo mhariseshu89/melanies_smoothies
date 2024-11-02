2ca281f9e1742a05208442c95fcf4658e449597c
mhariseshu89 <hari.mallela@fintinc.com>
2024-11-02 20:15:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,9 +58,7 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+
 
 
 
